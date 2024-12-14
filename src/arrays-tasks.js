7a04d84fdccc8ad25227bcd1df52ba2813888880
Margarita Vychik <aeternam.hiberna@gmail.com>
2024-12-14 16:54:21 +0300

    feat: add sumArrays solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,10 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  return arr1.length > arr2.length
+    ? arr1.map((value, i) => value + (arr2[i] || 0))
+    : arr2.map((value, i) => value + (arr1[i] || 0));
 }
 
 /**
